ce37f277dc2ce84efc59c46ac393904bfcc06c47
kedar2021 <kedar.joshi@plus.ai>
2025-10-12 21:47:00 -0700

    Basic unpack functions:
        - Works for bit widths <= 8 - need to expand to 16 or 32 bit integers
        - Need a better way to verify the pack/unpack functions - other than print

--- src/byte_pack.cpp
+++ src/byte_pack.cpp
@@ -9,18 +9,25 @@ int main () {
     in[2] = 1;
     in[3] = 1;
     in[4] = 1;
-    in[5] = 1;
+    in[5] = 100;
     in[6] = 2;
     in[7] = 3;
     in[8] = 3;
     in[9] = 3;
 
-    int bit_width = 8;
+    int bit_width = 7;
     int out_num = out_bytes_needed(bit_width, in.size());
     std::vector<uint8_t> out(out_num, 0);
     byte_pack (in.data(), out.data(), bit_width, in.size(), out_num);
 
     for (int i = 0; i < out.size(); i++) {
         std::cout << "out index " << i << " value is " << (int) out[i] << " \n";
+    }
+
+    std::vector<uint32_t> in_final(10);
+    byte_unpack (out.data(), in_final.data(), bit_width, in.size());
+
+    for (int i = 0; i < in_final.size(); i++) {
+        std::cout << "in_final index " << i << " value is " << (uint32_t) in_final[i] << " \n";
     }
 }
