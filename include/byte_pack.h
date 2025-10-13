f14115ddb37b1540bd7b15b845d591cf0007f534
Kedar Joshi <kedarjoshi7777@gmail.com>
2025-10-13 08:56:55 -0700

    Basic unpack functions: (#2)
    - Works for bit widths <= 8 - need to expand to 16 or 32 bit integers
    - Need a better way to verify the pack/unpack functions - other than print

--- include/byte_pack.h
+++ include/byte_pack.h
@@ -29,3 +29,20 @@ void byte_pack (const uint32_t* in, uint8_t* out, int bit_width, int in_num, int
         bit_pos = bit_pos + bit_width;
     }
 }
+
+void byte_unpack (const uint8_t* in, uint32_t* out, int bit_width, int in_num) {
+    int mask = (1 << bit_width) - 1;
+    int bit_pos = 0;
+    for (int i = 0; i < in_num; i++) {
+        int byte_index = (bit_pos >> 3);
+        int offset_index = (bit_pos & 0b111);
+
+        if (offset_index + bit_width > 8) {
+            int spillage = (offset_index + bit_width) & 0b111;
+            out[i] = (uint32_t) ((in[byte_index] << spillage) & mask) | ((in[byte_index+1] >> (8 - spillage)) & mask);
+        } else {
+            out[i] = (uint32_t) ((in[byte_index] >> ((offset_index + bit_width - 1) ^ 0b111)) & mask);
+        }
+        bit_pos = bit_pos + bit_width;
+    }
+}
